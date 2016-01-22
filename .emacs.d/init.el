f28c2f889362f5055073f8f8ff98db7f0e593dda
John Mercouris <john@mercouris.email>
2016-01-22 15:25:37 +0100

    emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,46 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs Configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; add melpa to packages
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+;; disable splash screen
+(setq inhibit-splash-screen t)
+;; disable menu bar
+(menu-bar-mode -1)
+;; disable tool bar
+(tool-bar-mode -1)
+;; set initial scratch bar message
+(setq initial-scratch-message nil)
+;; show parenthesis pairing
+(show-paren-mode 1)
+;; make buffer switch command auto suggestions, also for find-file command
+(ido-mode 1)
+;; make ido display choices vertically
+(setq ido-separator "\n")
+;; display any item that contains the chars you typed
+(setq ido-enable-flex-matching t)
+;; highlight current line
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#D3D3D3")
+;; truncate lines by default
+(set-default 'truncate-lines t)
+;; centered cursor mode
+(and
+ (require 'centered-cursor-mode)
+ (global-centered-cursor-mode +1))
+;; all back up files into same systemwide temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+;; enable elpy for python development
+(package-initialize)
+(elpy-enable)
+;; add sr-speedbar
+(require 'sr-speedbar)
