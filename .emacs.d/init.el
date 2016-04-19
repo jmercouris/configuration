e5b823209f0e664bc1a771f43f96e985d1c0ba31
John Mercouris <john@mercouris.email>
2016-04-19 14:00:32 +0200

    Removed automatic minimap configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,19 +72,3 @@
 
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-always-recenter t)
- '(minimap-highlight-line nil)
- '(minimap-mode t)
- '(minimap-window-location (quote right)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-active-region-background ((t (:background "gray97")))))
