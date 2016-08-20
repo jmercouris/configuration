9cdfbee268ac8f278ee1dd56944d13cad4709b7f
John Mercouris <john@mercouris.email>
2016-08-20 09:12:23 -0500

    Added centered point mode to git

---
+++ .emacs.d/center.el
@@ -0,0 +1,10 @@
+(define-minor-mode centered-point-mode
+  "Alaways center the cursor in the middle of the screen."
+  :lighter "..."
+  (cond (centered-point-mode (add-hook 'post-command-hook 'line-change))
+	(t (remove-hook 'post-command-hook 'line-change)))
+  )
+ 
+(defun line-change ()
+  (recenter)
+  )
