2b8cd68b30d79d902d0a8bc954c7a6f49e4d377b
John Mercouris <john@mercouris.email>
2016-09-28 22:22:06 -0500

    Updated Hydra to include magit and org mode navigation

--- .emacs.d/hydra.el
+++ .emacs.d/hydra.el
@@ -2,3 +2,31 @@
   "zoom"
   ("=" text-scale-increase "in")
   ("-" text-scale-decrease "out"))
+
+(defhydra hydra-magit (:color blue :columns 8)
+  "Magit"
+  ("c" magit-status "status")
+  ("C" magit-checkout "checkout")
+  ("v" magit-branch-manager "branch manager")
+  ("m" magit-merge "merge")
+  ("l" magit-log "log")
+  ("!" magit-git-command "command")
+  ("$" magit-process "process"))
+
+(defhydra hydra-org (:color red :hint nil)
+  "
+Navigation^
+---------------------------------------------------------
+_j_ next heading
+_k_ prev heading
+_h_ next heading (same level)
+_l_ prev heading (same level)
+_u_p higher heading
+_g_o to
+"
+  ("j" outline-next-visible-heading)
+  ("k" outline-previous-visible-heading)
+  ("h" org-forward-heading-same-level)
+  ("l" org-backward-heading-same-level)
+  ("u" outline-up-heading)
+  ("g" org-goto :exit t))
