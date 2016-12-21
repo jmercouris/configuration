6d43131e42f33c4a7c6129a6621f743c37048ec7
John Mercouris <john@mercouris.email>
2016-12-21 08:34:59 -0600

    Removed erase-buffer keybinding, no delete key on macbook keyboard

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -155,8 +155,6 @@
 (global-set-key (kbd "C-x o") 'switch-window)
 ;; golden-ratio should be called when switch-window is called
 (add-to-list 'golden-ratio-extra-commands 'switch-window)
-;; erase buffer with C-x + del
-(global-set-key (kbd "C-x <deletechar>") 'erase-buffer)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -168,9 +166,3 @@
     (rainbow-delimiters switch-window restclient find-file-in-repository multi-term web-mode undo-tree sphinx-doc perspective persp-mode neotree markdown-mode magit latex-preview-pane kivy-mode key-chord jinja2-mode hydra golden-ratio exec-path-from-shell elpy circe centered-cursor-mode auctex)))
  '(switch-window-qwerty-shortcuts (quote ("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o")))
  '(switch-window-shortcut-style (quote qwerty)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
