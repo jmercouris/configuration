b6494b902901ccd6dd3880f7285efbfad9c9f96c
John Mercouris <john@mercouris.email>
2017-01-06 18:33:08 -0600

    Updated init file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,6 @@
 set --export PATH /opt/local/Library/Frameworks/Python.framework/Versions/Current/bin $PATH
 set --export PATH /opt/local/bin /opt/local/sbin $PATH
 set --export PATH /Users/jmercouris/User $PATH
-set --export PATH $PATH /Library/TeX/texbin/
-set --export PATH $PATH /Applications/Postgres.app/Contents/Versions/9.5/bin
 set --export CLICOLOR "1"
 set --export VISUAL "emacs -nw"
 set --export EDITOR "$VISUAL"
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -198,7 +198,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Menlo"))))
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "nil" :family "Menlo"))))
  '(fringe ((t nil)))
  '(highlight-indentation-face ((t (:background "gray95"))))
  '(hiwin-face ((t (:background "gray95"))))
