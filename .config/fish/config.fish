b1b533a97305b0971f219b44c61c754ce4f7691e
John Mercouris <john@mercouris.email>
2017-01-21 22:12:30 -0600

    Simplified fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,14 @@
 set --export PATH /opt/local/Library/Frameworks/Python.framework/Versions/Current/bin $PATH
 set --export PATH /opt/local/bin /opt/local/sbin $PATH
 set --export PATH /Users/jmercouris/User $PATH
-set --export CLICOLOR "1"
-set --export VISUAL "emacs -nw"
-set --export EDITOR "$VISUAL"
 # Python Specific Environment Variables
 set --export VIRTUAL_ENV_DISABLE_PROMPT 1
 set --export PYTHONPATH /Users/jmercouris/Projects/scikit-learn
 set --export PYTHONPATH $PYTHONPATH /Users/jmercouris/Projects/AudioRead
-# Neo4j Database Location
-set --export NEO4J_REST_URL 'http://neo4j:passywordy@localhost:7474/db/data/'
 
+set --export CLICOLOR "1"
+set --export VISUAL "emacs -nw"
+set --export EDITOR "$VISUAL"
 
 # Colorize less things such as man pages
 set -x LESS_TERMCAP_mb (printf "\033[01;31m")
@@ -22,10 +20,6 @@ set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\033[0m")
 set -x LESS_TERMCAP_us (printf "\033[01;32m")
 
-# Get Battery Status
-alias battery='pmset -g batt'
-# Sleep
-alias sleep='pmset sleepnow'
 # Open Emacs in a terminal window
 alias e="emacs -nw"
 # Open Emacs in a graphical window
@@ -36,21 +30,15 @@ alias sudo='sudo '
 alias l='ls -la'
 # Get router IP
 alias router='netstat -rn |grep default'
-# Get CPU Temperature
-alias temperature='osx_cpu_temp'
 # Quit Terminal
 alias q='exit'
-# Spotlight functionality in Terminal
-alias spotlight='mdfind'
 # Screen Saver
 alias screen_saver='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine'
 # Alpine Custom Configuration File
 alias alpine='alpine -P ~/.pine.conf'
-# View Git History Graph
-alias git_graph='git log --graph --decorate --oneline'
 #Folder Shortcut Alias
 alias downloads='cd /Users/jmercouris/Downloads'
+alias academic='cd /Users/jmercouris/Documents/Academic'
 alias documents='cd /Users/jmercouris/Documents'
 alias projects='cd /Users/jmercouris/Projects'
 alias desktop='cd /Users/jmercouris/Desktop'
-
