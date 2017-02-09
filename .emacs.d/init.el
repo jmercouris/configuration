03ce0c220c12c1859c1760ad50130c346c7e24f9
John Mercouris <john@mercouris.email>
2017-02-09 13:06:15 +0100

    browse-kill-ring set as default

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -263,6 +263,8 @@ by using nxml's indentation rules."
 (defalias 'list-buffers 'ibuffer)
 ;; imenu anywhere binding
 (global-set-key (kbd "C-.") 'ido-imenu-anywhere)
+;; use browse-kill ring as the default for M-y
+(browse-kill-ring-default-keybindings)
 ;; ediff don't open new frame, split horiziontally
 ;; (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 ;; (setq ediff-split-window-function 'split-window-horizontally)
@@ -275,7 +277,7 @@ by using nxml's indentation rules."
  '(hiwin-mode t)
  '(package-selected-packages
    (quote
-    (imenu-anywhere py-isort which-key json-mode smooth-scrolling realgud exec-path-from-shell elpy hiwin smex avy rainbow-delimiters switch-window restclient find-file-in-repository multi-term web-mode undo-tree sphinx-doc perspective persp-mode neotree markdown-mode magit latex-preview-pane kivy-mode key-chord jinja2-mode hydra golden-ratio circe auctex)))
+    (browse-kill-ring imenu-anywhere py-isort which-key json-mode smooth-scrolling realgud exec-path-from-shell elpy hiwin smex avy rainbow-delimiters switch-window restclient find-file-in-repository multi-term web-mode undo-tree sphinx-doc perspective persp-mode neotree markdown-mode magit latex-preview-pane kivy-mode key-chord jinja2-mode hydra golden-ratio circe auctex)))
  '(realgud:pdb-command-name "python -m pdb")
  '(smooth-scroll-margin 15)
  '(smooth-scroll-strict-margins nil)
