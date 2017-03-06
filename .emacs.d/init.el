faded3dab3956c8a1de9c25ed8bbd9d788f54152
John Mercouris <john@mercouris.email>
2017-03-06 12:39:59 +0100

    Added function to copy buffer name into kill ring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -301,6 +301,28 @@ by using nxml's indentation rules."
   "Show the full path file name in the minibuffer."
   (interactive)
   (message (buffer-file-name)))
+;; copy buffer file name to kill ring
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copyies the buffer {name/mode}, file {name/full path/directory} to the kill-ring."
+  (interactive "cCopy (b) buffer name, (m) buffer major mode, (f) full buffer-file path, (d) buffer-file directory, (n) buffer-file basename")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?d)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          ((eq choice ?b)
+           (setq new-kill-string (buffer-name)))
+          ((eq choice ?m)
+           (setq new-kill-string (format "%s" major-mode)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
 
 ;; load Additional Files
 (load "~/.emacs.d/irc")
