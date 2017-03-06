c12656ccbe4c846a49cf3aed014744463b4ad347
John Mercouris <john@mercouris.email>
2017-03-06 23:41:52 +0100

    Counsel git grep included in ivy already

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,28 +51,6 @@
     (interactive)
     (ivy-set-action 'kill-buffer)
     (ivy-done)))
-;; Counsel git grep
-(defun counsel-git-grep-function (string &optional _pred &rest _u)
-  "Grep in the current git repository for STRING."
-  (split-string
-   (shell-command-to-string
-    (format
-     "git --no-pager grep --full-name -n --no-color -i -e \"%s\""
-     string))
-   "\n"
-   t))
-(defun counsel-git-grep ()
-  "Grep for a string in the current git repository."
-  (interactive)
-  (let ((default-directory (locate-dominating-file
-                             default-directory ".git"))
-        (val (ivy-read "pattern: " 'counsel-git-grep-function))
-        lst)
-    (when val
-      (setq lst (split-string val ":"))
-      (find-file (car lst))
-      (goto-char (point-min))
-      (forward-line (1- (string-to-number (cadr lst)))))))
 ;; bind to C-c j
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
 ;; show parenthesis pairing
