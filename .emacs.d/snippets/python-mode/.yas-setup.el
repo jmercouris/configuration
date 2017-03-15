7fa1ecb221a069afa2d22227dbd8f6ec03ea0958
John Mercouris <john@mercouris.email>
2017-03-15 11:36:38 +0100

    improved google style docstring function

--- .emacs.d/snippets/python-mode/.yas-setup.el
+++ .emacs.d/snippets/python-mode/.yas-setup.el
@@ -34,18 +34,26 @@
                        "\nReturns\n-------" formatted-ret)
                  "\n"))))
 
-(defun python-args-to-docstring-google ()
-  "return docstring format for the python arguments in yas-text"
-  (let* ((args (python-split-args yas-text))
-         (format-arg (lambda(arg)
-                       (concat "        " (nth 0 arg) ": " (if (nth 1 arg) ", optional"))))
-         (formatted-params (mapconcat format-arg args "\n"))
-         (formatted-ret (mapconcat format-arg (list (list "out")) "\n")))
-    (unless (string= formatted-params "")
-      (mapconcat 'identity
-                 (list "\nArgs:" formatted-params
-                       "\nReturn:" formatted-ret)
-                 "\n"))))
+(defun python-args-to-google-docstring (text &optional make-fields)
+  "Return a reST docstring format for the python arguments in yas-text."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args text))
+     (nr 0)
+         (formatted-args
+      (mapconcat
+       (lambda (x)
+         (concat "   " (nth 0 x)
+		 ":"
+		 (if make-fields (format " ${%d:arg%d}" (incf nr) nr))
+		 (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
+       args
+       indent)))
+    (unless (string= formatted-args "")
+      (concat
+       (mapconcat 'identity
+          (list "" "Args:" formatted-args)
+          indent)
+       "\n"))))
 
 (add-hook 'python-mode-hook
           '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
