ac4a06dbf52680bad29866497981d68c4a464cd8
John Mercouris <john@mercouris.email>
2017-06-15 09:33:07 +0200

    Formatting changes

--- .emacs.d/develop/_python.el
+++ .emacs.d/develop/_python.el
@@ -10,36 +10,30 @@
 
 (setq realgud:pdb-command-name "python -m pdb")
 
-;; python shell prompt warning
 (setq python-shell-prompt-detect-failure-warning nil)
 
 (defun python-shell ()
     (interactive)
   (run-python)
-  (python-shell-switch-to-shell)
-  )
+  (python-shell-switch-to-shell))
 
 (defun python-outline ()
   (interactive)
-  (occur "class \\|def ")
-  )
+  (occur "class \\|def "))
 
 (defun python-class-outline ()
   (interactive)
-  (occur "class ")
-  )
+  (occur "class "))
 
 (defun pyrm ()
   (interactive)
   (setq command (concatenate 'string "autoflake --in-place --remove-unused-variables " buffer-file-name))
   (shell-command command)
   ;; Reload the modified file
-  (revert-buffer t t)
-  )
+  (revert-buffer t t))
 
 (defun python-django-test ()
   (interactive)
   (kill-new
    (concatenate 'string "./manage.py test "
-		(replace-in-string (projectile-project-root) "" (buffer-file-name)))
-))
+		(replace-in-string (projectile-project-root) "" (buffer-file-name)))))
