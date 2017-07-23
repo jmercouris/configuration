bf44aca59d46767bd7928bccc03a1ec242d49070
John Mercouris <john@mercouris.email>
2017-07-24 00:33:19 +0300

    Updated next init file to swap command and control keys

--- .next.d/init.lisp
+++ .next.d/init.lisp
@@ -1,5 +1,12 @@
 (in-package :next)
 
+;; swap command and control keys on OSX
+;; so that control ACTUALLY represents control
+(let ((original_control *control-key*)
+      (original_command *meta-key*))
+  (setf *control-key* original_command)
+  (setf *meta-key* original_control))
+
 (defun print-text ()
   (print "hey"))
 
