5bd0691b1e0554e37cbfa799250f8ef31f71afa7
John Mercouris <john@mercouris.email>
2017-08-12 19:45:05 +0200

    updated next hotkeys to osx / emacs standard

--- .next.d/init.lisp
+++ .next.d/init.lisp
@@ -1,13 +1,9 @@
 (in-package :next)
 
-;; swap command and control keys on OSX
-;; so that control ACTUALLY represents control
+;; swap keys for consistency with other non-qt programs
 (let ((original_control *control-key*)
-      (original_command *meta-key*))
-  (setf *control-key* original_command)
-  (setf *meta-key* original_control))
-
-(defun print-text ()
-  (print "change window"))
-
-(define-key global-map (kbd "C-x o") #'print-text)
+      (original_meta *meta-key*)
+      (original_alt *alt-key*))
+  (setf *control-key* original_meta)
+  (setf *meta-key* original_alt)
+  (setf *super-key* original_control))
