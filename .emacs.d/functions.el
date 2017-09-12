41fd5ce5a30b886a29f78d08650cfaeab2ab2799
John Mercouris <john@mercouris.email>
2017-09-12 11:39:09 -0500

    added xpath caclucation functions

--- .emacs.d/functions.el
+++ .emacs.d/functions.el
@@ -169,3 +169,21 @@ This command is convenient when reading novel, documentation."
   (ivy-mode -1)
   (call-interactively 'rgrep)
   (ivy-mode))
+
+(defun nxml-where ()
+  "Display the hierarchy of XML elements the point is on as a path."
+  (interactive)
+  (let ((path nil))
+    (save-excursion
+      (save-restriction
+	(widen)
+	(while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
+		    (condition-case nil
+			(progn
+			  (nxml-backward-up-element) ; always returns nil
+			  t)
+		      (error nil)))
+	  (setq path (cons (xmltok-start-tag-local-name) path)))
+	(if (called-interactively-p t)
+	    (message "/%s" (mapconcat 'identity path "/"))
+	  (format "/%s" (mapconcat 'identity path "/")))))))
