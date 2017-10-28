ab316a6c2a98e4547045e0787ca6f0d20e2f51f9
John Mercouris <john@mercouris.email>
2017-10-28 16:50:03 +0200

    changed default cl to ccl

--- .emacs.d/develop/_lisp.el
+++ .emacs.d/develop/_lisp.el
@@ -1,7 +1,7 @@
 (load (expand-file-name "~/.quicklisp/slime-helper.el"))
 (load "~/.quicklisp/clhs-use-local.el" t)
 
-(setq inferior-lisp-program "ecl")
+(setq inferior-lisp-program "ccl")
 (slime-setup '(slime-fancy slime-company))
 
 (add-hook 'lisp-mode-hook 'highlight-indent-guides-mode)
