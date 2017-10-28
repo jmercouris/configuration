d3d90dad821ce4bd595790c18ee1ee2927d404dc
John Mercouris <john@mercouris.email>
2017-10-28 16:49:40 +0200

    removed swapping of modifier keys, as it is now default osx behavior

--- .next.d/init.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :next)
-
-;; swap keys for consistency with other non-qt programs
-(let ((original_control *control-key*)
-      (original_meta *meta-key*)
-      (original_alt *alt-key*))
-  (setf *control-key* original_meta)
-  (setf *meta-key* original_alt)
-  (setf *super-key* original_control))
