7f19f3a3f6e696f791a7fb6cf7894feac97dba37
John Mercouris <john@mercouris.email>
2017-11-02 14:48:01 +0100

    remove eshell history prompts

--- .emacs.d/eshell.el
+++ .emacs.d/eshell.el
@@ -32,26 +32,3 @@
     (insert (concat "ls"))
     (eshell-send-input)))
 (bind-key "C-!" 'eshell-here)
-(defun eshell-next-prompt (n)
-  "Move to end of Nth next prompt in the buffer. See `eshell-prompt-regexp'."
-  (interactive "p")
-  (re-search-forward eshell-prompt-regexp nil t n)
-  (when eshell-highlight-prompt
-    (while (not (get-text-property (line-beginning-position) 'read-only) )
-      (re-search-forward eshell-prompt-regexp nil t n)))
-  (eshell-skip-prompt))
-(defun eshell-previous-prompt (n)
-  "Move to end of Nth previous prompt in the buffer. See `eshell-prompt-regexp'."
-  (interactive "p")
-  (backward-char)
-  (eshell-next-prompt (- n)))
-(defun eshell-insert-history ()
-  "Displays the eshell history to select and insert back into your eshell."
-  (interactive)
-  (insert (ido-completing-read "Eshell history: "
-                               (delete-dups
-                                (ring-elements eshell-history-ring)))))
-(add-hook 'eshell-mode-hook (lambda ()
-    (define-key eshell-mode-map (kbd "M-S-P") 'eshell-previous-prompt)
-    (define-key eshell-mode-map (kbd "M-S-N") 'eshell-next-prompt)
-    (define-key eshell-mode-map (kbd "M-r") 'eshell-insert-history)))
