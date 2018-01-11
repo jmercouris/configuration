543b5cc6bcfce67b884baacec8ac3b9b1c44f2d7
John Mercouris <john@mercouris.email>
2018-01-11 12:09:47 -0600

    update starting irc channels

--- .emacs.d/irc.el
+++ .emacs.d/irc.el
@@ -5,7 +5,7 @@
          :nick ,freenode-nick
          :sasl-username ,freenode-username
          :sasl-password ,freenode-password
-	 :channels ("#lisp" "#lispcafe" "##nEXT-Browser" "#ccl")
+	 :channels ("#lisp" "#lispcafe" "#next-browser" "#ccl")
 	 :port "6697")))
 
 (setq circe-default-part-message "Exit.")
