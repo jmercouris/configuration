c100029746125093c7e3512fe7408aaa732d2b5f
John Mercouris <john@mercouris.email>
2018-06-27 14:04:56 +0200

    add swank file

---
+++ .swank.lisp
@@ -0,0 +1,6 @@
+(setf swank:*globally-redirect-io* t)
+(setf swank:*communication-style* :spawn)
+;; sometimes need to set it to this depends on system
+;; (setf swank:*communication-style* :fd-handler) 
+
+
