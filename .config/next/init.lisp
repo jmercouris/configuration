ca7adfd9a766ee22f857b0a0e65a8ef6aef87361
John Mercouris <john@mercouris.email>
2018-11-08 10:08:58 +0100

    remove unused commands

--- .config/next/init.lisp
+++ .config/next/init.lisp
@@ -2,12 +2,3 @@
 
 (setf *start-page-url* "about:blank")
 
-(define-command show-minibuffer ()
-  (minibuffer-set-height *interface* (window-active *interface*) 100))
-
-(define-command hide-minibuffer ()
-  (minibuffer-set-height *interface* (window-active *interface*) 10))
-
-(define-key *global-map* (kbd "C-q") 'show-minibuffer)
-(define-key *global-map* (kbd "M-q") 'hide-minibuffer)
-
