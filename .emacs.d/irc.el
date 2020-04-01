5ee64afa2275fe7794a644a976429b057d7c2bf2
John Mercouris <john@mercouris.email>
2020-04-01 17:53:26 +0200

    irc.el: add channels

--- .emacs.d/irc.el
+++ .emacs.d/irc.el
@@ -5,13 +5,18 @@
          :nick ,freenode-nick
          :sasl-username ,freenode-username
          :sasl-password ,freenode-password
-	 :channels ("#lisp" "#next-browser" "#startups" "#mezzano")
+	 :channels ("#lisp" "#ccl" "#next-browser" "#startups" "#mezzano")
 	 :port "6697")
         ("ircnet"
          :host "irc.ircnet.com"
          :tls t
          :nick ,freenode-nick
          :channels ("#worldchat")
+         :port "6697")
+        ("gnome"
+         :host "irc.gnome.org"
+         :tls t
+         :nick ,freenode-nick
          :port "6697")))
         
 (setq circe-default-part-message "Exit.")
